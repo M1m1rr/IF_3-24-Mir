3cc200a7699d0c5f6a3b9624924417d0cec2ae70
Mimir <119404804+M1m1rr@users.noreply.github.com>
2023-08-08 09:38:58 +0700

    Add files via upload

--- Python_Sem5_2023/fruit_shop.py
+++ Python_Sem5_2023/fruit_shop.py
@@ -1,40 +1,53 @@
-#Cock
-
 print('--------------------------')
-print('-----------Buah-----------')
+print('--------Toko Buah---------')
 print('--------------------------')
+nama = input('Nama Pembeli : ')
 buah = input('Buah apa : ')
-if buah : 'apel'
-harga = 6500
-if buah : 'semangka'
-harga = 7500
-if buah : 'alpukat'
-harga = 4500
-if buah : 'jambu'
-harga = 5500
-if buah : 'melon'
-harga = 10500
 
-print('Harga', harga)
-jml = input('Berapa : ')
 
-buah1 = input('Tambahan Lain (Ya/Tidak)')
-if buah1 : 'Ya'
-buah = input('Masukkan Buah : ')
+if (buah == 'apel'):
+ harga = 6500
+if (buah == 'semangka'):
+ harga = 7500
+if (buah == 'alpukat'):
+ harga = 4500
+if (buah == 'jambu'):
+ harga = 5500
+if (buah == 'melon'):
+ harga = 10500
+if (buah == 'pisang'):
+ harga = 5000
 
-if buah1 : 'Tidak'
+print('Harga per kg : ', harga)
+
+jml = input('Berapa kg : ')
+
 total = int(jml)*int(harga)
-
 print('Totalnya : ', int(total))
-
+    
+if total <= 47500:
+    print('Anda tidak mendapatkan diskon')
+    
+if total >= 50000:
+    diskon = (int(total)/100)*5
+    print('Anda mendapat diskon 5% sebesar : ', diskon)
+    total1 = int(total)-int(diskon)
+    print('Total harga : ', total1)
+    
+if total >= 100000:
+    diskon = (int(total)/100)*20
+    print('Anda mendapat diskon 20% sebesar : ', diskon)
+    total1 = int(total)-int(diskon)
+    print('Total harga : ', total1)
+    
 uangmu = int(input('Uangmu berapa : '))
-kembalian = int(total)-int(uangmu)
+kembalian = int(uangmu)-int(total1)
 
-if uangmu < total:
-    print('Miskin Kau')
+if uangmu < total1:
+    print('Uang Tidak Cukup')
 
-elif uangmu > total:
+elif uangmu > total1:
  print('Kembalianmu : ', int(kembalian))
  
-elif uangmu == total:
- print('Uang Pas')
+elif uangmu == total1:
+ print('Uang Pas, Terimakasih sudah berbelanja!')
